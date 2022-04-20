8a79bb6a0da8bcb730aaa63ce63bff81028d0ade
Jon Connell <con18013@byui.edu>
2022-04-20 09:55:08 -0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace JamesBond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your first name? ");
+            string first_name = Console.ReadLine();
+
+            Console.Write("What is your last name? ");
+            string last_name = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
+        }
+    }
+}
